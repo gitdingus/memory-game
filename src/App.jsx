d59377821d9da8cc4c6ad012f328ea8b5f22efbc
Josh S <aaabccc87@protonmail.com>
2022-10-21 21:21:54 -0400

    Import styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import getCardInfo from './card-info.js';
 import Card from './components/Card.jsx';
+import './styles.css';
 
 export default function App() {
   // Held in a variable so they are able to be shuffled outside of the return
