836a0cad38b9d2b9414dca22abbf913e78c6d641
Josh S <aaabccc87@protonmail.com>
2022-10-21 21:15:16 -0400

    Display cards to make sure everything is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import getCardInfo from './card-info.js';
+import Card from './components/Card.jsx';
 
 export default function App() {
+  // Held in a variable so they are able to be shuffled outside of the return
+  const Cards = getCardInfo().map((card) => (
+    <Card key={card.uuid} title={card.title} path={card.path} />
+  ));
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      { Cards }
+    </div>
   );
 }
