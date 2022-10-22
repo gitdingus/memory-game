118fd6b3f980e0050fe5f02dc2173897989225a4
Josh S <aaabccc87@protonmail.com>
2022-10-22 12:29:45 -0400

    Game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import getCardInfo from './card-info.js';
+import shuffleArray from './shuffle-array.js';
 import Card from './components/Card.jsx';
 import './styles.css';
 
+let cardData = getCardInfo();
+
 export default function App() {
-  // Held in a variable so they are able to be shuffled outside of the return
-  const Cards = getCardInfo().map((card) => (
-    <Card key={card.uuid} title={card.title} path={card.path} />
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const pScore = document.querySelector('#score');
+    pScore.textContent = `Score: ${score}`;
+  });
+
+  const cardClicked = (uuid, previouslyClicked) => {
+    if (previouslyClicked === true) {
+      setScore(0);
+      cardData = getCardInfo();
+    } else {
+      setScore(score + 1);
+    }
+  };
+
+  const Cards = cardData.map((card) => (
+    <Card
+      key={card.uuid}
+      uuid={card.uuid}
+      title={card.title}
+      path={card.path}
+      reportClick={cardClicked}
+    />
   ));
+
+  shuffleArray(Cards);
 
   return (
     <div className="card-container">
