d311ede69fa9d05294d0554c028b6919e7257048
Josh S <aaabccc87@protonmail.com>
2022-10-22 11:24:12 -0400

    Return button instead of div, handle click events

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function Card(props) {
-  const { uuid, title, path } = props;
+  const {
+    uuid,
+    title,
+    path,
+    reportClick,
+  } = props;
+  const [previouslyClicked, setPreviouslyClicked] = useState(false);
+
+  function handleClick() {
+    reportClick(uuid, previouslyClicked);
+    setPreviouslyClicked(true);
+  };
+
   return (
-    <div key={uuid} className="card">
+    <button key={uuid} type="button" className="card" onClick={handleClick}>
       <img src={path} alt={title} />
       <h1>{title}</h1>
-    </div>
+    </button>
   );
 }
 
@@ -15,12 +27,14 @@ Card.propTypes = {
   uuid: PropTypes.string,
   title: PropTypes.string,
   path: PropTypes.string,
+  reportClick: PropTypes.func,
 };
 
 Card.defaultProps = {
   uuid: '',
   title: '',
   path: '',
+  reportClick: () => {},
 };
 
 export default Card;
